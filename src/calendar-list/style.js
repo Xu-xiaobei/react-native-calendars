cbae3faeb40835e1be44486b0b56ecb61b2aa1f9
Xu-xiaobei <canchonghanye@163.com>
2018-07-24 19:28:16 +0800

    日历修复水平方向滑动

---
+++ src/calendar-list/style.js
@@ -0,0 +1,55 @@
+import {StyleSheet,Platform} from 'react-native';
+import * as defaultStyle from '../style';
+
+const STYLESHEET_ID = 'stylesheet.calendar-list.main';
+
+export default function getStyle(theme = {}) {
+  const appStyle = { ...defaultStyle, ...theme };
+  return StyleSheet.create({
+    container: {
+      backgroundColor: appStyle.calendarBackground,
+    },
+    placeholder: {
+      backgroundColor: appStyle.calendarBackground,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    placeholderText: {
+      fontSize: 30,
+      fontWeight: '200',
+      color: appStyle.dayTextColor,
+    },
+    monthText: {
+      fontSize: appStyle.textMonthFontSize,
+      fontFamily: appStyle.textMonthFontFamily,
+      fontWeight: appStyle.textMonthFontWeight,
+      color: appStyle.monthTextColor,
+      margin: 10,
+    },
+    arrow: {
+      padding: 10,
+    },
+    arrowImage: {
+      ...Platform.select({
+        ios: {
+          tintColor: appStyle.arrowColor,
+        },
+        android: {
+          tintColor: appStyle.arrowColor,
+        },
+      }),
+    },
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingLeft: 10,
+      paddingRight: 10,
+      alignItems: 'center',
+    },
+    calendar: {
+      paddingLeft: 15,
+      paddingRight: 15,
+    },
+    ...(theme[STYLESHEET_ID] || {}),
+  });
+}
